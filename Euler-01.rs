8fe325ff25cd3b02f4c13976e0b7e7a35a9b52dd
Steve Anton <anxuiz.nx@gmail.com>
2014-06-25 10:06:00 -0700

    Work with the latest rust nightly

--- Euler-01.rs
+++ Euler-01.rs
@@ -1,6 +1,6 @@
 fn main() {
 	let mut sum = 0;
-	for i in range(1, 1000) {
+	for i in range(1u, 1000) {
 		if i % 3 == 0 || i % 5 == 0 {
 			sum += i;
 		}
--- Euler-02.rs
+++ Euler-02.rs
@@ -2,7 +2,7 @@ fn main() {
 	let mut n1 = 1;
 	let mut n2 = 2;
 	let mut sum = 2;
-	while n1 + n2 <= 4_000_000 {
+	while n1 + n2 <= 4_000_000u {
 		let next = n1 + n2;
 		if next % 2 == 0 {
 			sum += next;
