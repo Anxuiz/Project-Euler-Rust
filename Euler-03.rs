993264f68b858bf8d9ba6b2812a0a13d9c2ff176
Steve Anton <anxuiz.nx@gmail.com>
2014-10-01 23:19:24 -0700

    Fix Euler 03

--- Euler-03.rs
+++ Euler-03.rs
@@ -7,16 +7,16 @@ fn max<T:Ord>(a: T, b: T) -> T {
 }
 
 fn main() {
-	let mut n = 600_841_475_143u64;
-	let mut largestFactor = 1;
+	let mut n = 600_851_475_143u64;
+	let mut largest_factor = 1;
 	let mut factor = 2;
 	while n > 1 {
 		if n % factor == 0 {
 			n /= factor;
-			largestFactor = max(largestFactor, factor);
+			largest_factor = max(largest_factor, factor);
 		} else {
 			factor += 1;
 		}
 	}
-	println!("{}", largestFactor);
+	println!("{}", largest_factor);
 }
